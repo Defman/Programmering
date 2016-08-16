c4c8335adce36129e0bd6ee9810f1fe6113b483d
Jacob <Jacob@Rosborg.dk>
2016-08-16 12:09:58 +0200

    Quadratic solver

---
+++ Quadratic.py
@@ -0,0 +1,16 @@
+def discriminant(a, c, b):
+    return 4*a*c+b**2
+
+
+def quadratic(a, c, b):
+    if a == 0:
+        return ()
+    d = discriminant(a, c, b)
+    if d < 0:
+        return ()
+    return (-b + d ** (1 / 2)) / 2 * a, (-b - d ** (1 / 2)) / 2 * a
+
+
+a, b, c = map(float, input("a, b, c: ").split(","))
+
+print(quadratic(a, b, c))
